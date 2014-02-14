85b257cc03b813204e590d1381a0b7659b826062
Alec Sun <iggy1337@gmail.com>
2014-02-13 22:34:53 -0500

    base

---
+++ gen/sparkjolt/exonian/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package sparkjolt.exonian;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/sparkjolt/exonian/Article.java
@@ -0,0 +1,36 @@
+package sparkjolt.exonian;
+
+import java.util.Locale;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.NavUtils;
+import android.support.v4.view.ViewPager;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Article extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.article);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.home, menu);
+		return true;
+	}
+
+
+}
