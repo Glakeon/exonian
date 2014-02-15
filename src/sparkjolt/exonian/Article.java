4a4e0984fbf1bc0822e174adb9da0b57bfc2efbe
Alec Sun <iggy1337@gmail.com>
2014-02-14 20:39:28 -0500

    see vogella json

--- src/sparkjolt/exonian/Article.java
+++ src/sparkjolt/exonian/Article.java
@@ -1,21 +1,19 @@
 package sparkjolt.exonian;
 
-import java.util.Locale;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.NavUtils;
-import android.support.v4.view.ViewPager;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class Article extends FragmentActivity {
 
@@ -31,6 +29,30 @@ public class Article extends FragmentActivity {
 		getMenuInflater().inflate(R.menu.home, menu);
 		return true;
 	}
+	
+	// Using the StringBuilder class, returns the JSON as a string
+	public String readExonianFeed() {
+		StringBuilder builder = new StringBuilder();
+		HttpClient client = new DefaultHttpClient();
+		HttpGet httpGet = new HttpGet("http://theexonian.com/new/2014/02/13/rdfz-students-come-to-campus/?json=1");
+		try {
+			HttpResponse response = client.execute(httpGet);
+			StatusLine statusLine = response.getStatusLine();
+			int statusCode = statusLine.getStatusCode();
+			if (statusCode == 200) {
+				HttpEntity entity = response.getEntity();
+				InputStream content = entity.getContent();
+				BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+				String line;
+				while ((line = reader.readLine()) != null) {
+					builder.append(line);
+				}
+			} else {
 
-
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return builder.toString();
+	}
 }
