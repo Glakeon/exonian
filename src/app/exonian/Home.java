b042c2a391291684d6e6eae3c7125ec52ff4f986
Alec Sun <iggy1337@gmail.com>
2014-02-17 22:00:34 -0500

    start with home page

--- src/app/exonian/Home.java
+++ src/app/exonian/Home.java
@@ -1,14 +1,31 @@
 package app.exonian;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
 import java.util.Locale;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
@@ -16,6 +33,7 @@ import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class Home extends FragmentActivity {
 
@@ -56,9 +74,9 @@ public class Home extends FragmentActivity {
 		@Override
 		public Fragment getItem(int position) {
 			// Called to instantiate the fragment for the given page.
-			Fragment fragment = new SectionFragment();
+			Fragment fragment = new NewsFragment();
 			Bundle args = new Bundle();
-			args.putInt(SectionFragment.SECTION_NUMBER, position + 1);
+			args.putInt(NewsFragment.SECTION_NUMBER, position + 1);
 			fragment.setArguments(args);
 			return fragment;
 		}
@@ -84,7 +102,7 @@ public class Home extends FragmentActivity {
 		}
 	}
 	
-	public static class SectionFragment extends Fragment {
+	public static class NewsFragment extends Fragment {
 		public static final String SECTION_NUMBER = "section_number";
 		
 		@Override
@@ -98,6 +116,116 @@ public class Home extends FragmentActivity {
 			rootView.addView(button);
 			return rootView;
 		}
-	}
+		
+		public class DownloadNews extends AsyncTask<String, Void, String> {
+			
+			private Activity activity;
+			
+			public DownloadNews(Activity activity) {
+				this.activity = activity;
+			}
+			
+			// Reads an InputStream and converts it to a string
+			public String readIt(InputStream stream) {
+				try {
+					BufferedReader reader = new BufferedReader(new InputStreamReader(stream, "utf-8"), 8);
+					StringBuilder sb = new StringBuilder();
+					String line = null;
+					while ((line = reader.readLine()) != null) {
+						sb.append(line + "\n");
+					}
+					stream.close();
+					return sb.toString();
+				} catch (IOException e) {
+					return "Error loading the article.";
+				}
+			}
 
+			@Override
+			protected String doInBackground(String... urls) {
+				InputStream is = null;
+				try {
+					// The URL is the first in the array
+					URI url = URI.create(urls[0]);
+					HttpClient httpclient = new DefaultHttpClient();
+					HttpGet httpget = new HttpGet(url);
+					HttpResponse response = httpclient.execute(httpget);
+					HttpEntity entity = response.getEntity();
+					
+					// Starts the query
+					is = entity.getContent();
+					if (response.getStatusLine().getStatusCode() == 200) {
+						
+						// Convert the InputStream into a string
+						String contentAsString = readIt(is);
+						return contentAsString;
+					} else {
+						return "Error loading the article.";
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				} finally {
+					// Close the InputStream
+					if (is != null) {
+						try {
+							is.close();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+				return "Error loading the article.";
+			}
+
+			@Override
+			protected void onPostExecute(String result) {
+				try {
+					// Construct a JSON object and get the content of the post
+					JSONObject jsonObject = new JSONObject(result);
+					String contentText = (String) ((JSONObject) jsonObject.get("post")).get("content");
+					final String titleText = (String) ((JSONObject) jsonObject.get("post")).get("title");
+					final String authorText = (String) ((JSONObject) ((JSONObject) jsonObject.get("post")).get("author")).get("name");
+					final String dateText = (String) ((JSONObject) jsonObject.get("post")).get("date");
+					
+					// Set the font to Ebrima
+					final Typeface tf = Typeface.createFromAsset(activity.getAssets(), "fonts/ebrima.ttf");
+					
+					activity.runOnUiThread(new Runnable() {
+
+						@Override
+						public void run() {
+							((TextView) activity.findViewById(R.id.article_title)).setText(titleText);
+							((TextView) activity.findViewById(R.id.article_author)).setText(authorText);
+							((TextView) activity.findViewById(R.id.article_date)).setText(dateText);
+
+							((TextView) activity.findViewById(R.id.article_content)).setTypeface(tf);
+							((TextView) activity.findViewById(R.id.article_title)).setTypeface(tf, Typeface.BOLD);
+							((TextView) activity.findViewById(R.id.article_author)).setTypeface(tf, Typeface.ITALIC);
+						}
+
+					});
+
+					// Do not get the CSS after the </p> tag for the content text
+					contentText = contentText.substring(0, contentText.indexOf("<style type='text/css'>"));
+					contentText = contentText.substring(0, contentText.lastIndexOf("</p>"));
+
+					final String finalContent = contentText;
+
+					// Run the UI changes in the UI thread per thread policy.
+					activity.runOnUiThread(new Runnable() {
+
+						@Override
+						public void run() {
+							((TextView) activity.findViewById(R.id.article_content)).setText(Html.fromHtml(finalContent));
+						}
+
+					});
+					// articles.insertArticle(titleText, authorText, finalContent, dateText);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+	}
 }
