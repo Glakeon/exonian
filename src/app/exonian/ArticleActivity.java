492b28bac8aa489d636b92a687763a9fbc60f3cf
Alec Sun <iggy1337@gmail.com>
2014-02-18 16:06:06 -0500

    list adapter working on

--- src/app/exonian/ArticleActivity.java
+++ src/app/exonian/ArticleActivity.java
@@ -186,7 +186,7 @@ public class ArticleActivity extends FragmentActivity {
 					}
 
 				});
-				Log.d("exeter", Long.toString(db.insertArticle(titleText, authorText, finalContent, dateText)));
+				// Log.d("Database", Long.toString(db.insertArticle(titleText, authorText, finalContent, dateText)));
 			} catch (JSONException e) {
 				e.printStackTrace();
 			}
