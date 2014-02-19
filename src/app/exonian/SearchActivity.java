2523952e068cff08f32d6002b9d1789a4eec2d06
Alec Sun <iggy1337@gmail.com>
2014-02-19 11:30:57 -0500

    cheddar cheese

--- src/app/exonian/SearchActivity.java
+++ src/app/exonian/SearchActivity.java
@@ -9,23 +9,19 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
 
-/**
- * @author Weihang Fan
- *
- */
 public class SearchActivity extends ListActivity {
 
 	private TextWatcher filterTextWatcher = new TextWatcher() {
 
 		public void afterTextChanged(Editable s) {
+		
 		}
 
-		public void beforeTextChanged(CharSequence s, int start, int count,
-				int after) {
+		public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 		}
 
-		public void onTextChanged(CharSequence s, int start, int before,
-				int count) {
+		public void onTextChanged(CharSequence s, int start, int before, int count) {
+		
 		}
 
 	};
